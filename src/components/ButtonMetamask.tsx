97d9da7bbce5f49e294b5785fad7ece5be4a61db
heronlancellot <heronlancellot@id.uff.br>
2023-04-03 23:23:58 -0300

    Feat: Change the Chains to 'goerli & celoAlfajores'

--- src/components/ButtonMetamask.tsx
+++ src/components/ButtonMetamask.tsx
@@ -1,14 +1,14 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import { goerli, celoAlfajores } from 'wagmi/chains'
 import { Web3Button } from '@web3modal/react'
 
 function HomePage() {
   return <Web3Button />
 }
 
-const chains = [arbitrum, mainnet, polygon]
+const chains = [goerli, celoAlfajores]
 const projectId = '47199843a3bf2f392f83c86dd83b7ee0'
 
 const { provider } = configureChains(chains, [w3mProvider({ projectId })])
